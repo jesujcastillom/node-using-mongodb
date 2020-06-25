ce7fb8647743be2f9415a716e1d562553437cb0e
Jesu Castillo <jcastillo@MacBook-Pro-van-Jesu.local>
2020-06-25 18:57:39 +0200

    Completed module

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,15 +1,32 @@
-import { ProductSchema } from "../models/models";
-import mongoose from "mongoose";
+import { ProductSchema } from '../models/models';
+import mongoose from 'mongoose';
 
-const Product = mongoose.model("Product", ProductSchema);
+const Product = mongoose.model('Product', ProductSchema);
 
 export const addnewProduct = (req, res) => {
   const newProduct = new Product(req.body);
   newProduct.save((err, Product) => {
     if (err) {
-      res.status(400).json(err).end();
-      return;
+      return res.status(400).json(err).end();
     }
     res.status(200).json(Product);
+  });
+};
+
+export const getProducts = (req, res) => {
+  Product.find({}, (err, Product) => {
+    if (err) {
+      return res.status(400).end();
+    }
+    res.json(Product);
+  });
+};
+
+export const getProductWithID = (req, res) => {
+  Product.findById(req.params.ProductID, (err, Product) => {
+    if (err) {
+      return res.status(404).end();
+    }
+    res.json(Product);
   });
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,10 @@
-import { addnewProduct } from '../controllers/controllers';
+import {
+  addnewProduct,
+  getProducts,
+  getProductWithID,
+} from '../controllers/controllers';
 
 export function routes(app) {
-  app.route('/products').post(addnewProduct);
+  app.route('/products').post(addnewProduct).get(getProducts);
+  app.route('/products/:ProductID').get(getProductWithID);
 }
